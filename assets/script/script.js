e96b89ddb48dc5f21743c3ba6c932c2d905d6b93
bernardbdas <bernarddas1@gmail.com>
2022-01-28 01:16:47 +0530

    commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,61 +1,59 @@
-function clearErrors() {
+const form = document.getElementById('form');
+const uname = document.getElementById('username');
+const email = document.getElementById('email');
+const phn = document.getElementById('contact');
+const pass = document.getElementById('pass');
+const cpass = document.getElementById('cpass');
 
-    errors = document.getElementsByClassName('formerror');
-    for (let item of errors) {
-        item.innerHTML = "";
+//add event
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validate();
+})
+
+inputBox.addEventListener('keydown', function(e) {
+    var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    if (nums.includes(e.key)) {
+        e.preventDefault();
+        setErrormsg(uname, 'username cannot contain numbers')
+    }
+})
+
+//defining isEmail()
+const isEmail = (emailVal) => {
+    return String(emailVal).match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+
+//defining validate function
+
+const validate = () => {
+    const uname = uname.value.trim();
+    const email = email.value.trim().toLowerCase();
+    const phn = phn.value.trim();
+    const pass = pass.value.trim();
+    const cpass = cpass.value.trim();
+
+    //validate username
+
+
+
+    if (uname === "") {
+        setErrormsg(uname, 'username cannot be blank');
+    } else
+    if (uname.length <= 2) {
+        setErrormsg(uname, 'username must contain atleast 3 characters');
+    } else {
+        setSuccessmsg(uname);
     }
 
-
-}
-
-function seterror(id, error) {
-    //sets error inside tag of id 
-    element = document.getElementById(id);
-    element.getElementsByClassName('formerror')[0].innerHTML = error;
-
-}
-
-function validateForm() {
-    var returnval = true;
-    clearErrors();
-
-    //perform validation and if validation fails, set the value of returnval to false
-    var name = document.forms['myForm']["fname"].value;
-    if (name.length < 5) {
-        seterror("name", "*Length of name is too short");
-        returnval = false;
+    //validate email
+    if (email === "") {
+        setErrormsg(email, 'email cannot be blank');
+    } else
+    if (!isEmail(email)) {
+        setErrormsg(email, 'Not a valid Email');
+    } else {
+        setSuccessmsg(email);
     }
-
-    if (name.length == 0) {
-        seterror("name", "*Length of name cannot be zero!");
-        returnval = false;
-    }
-
-    var email = document.forms['myForm']["femail"].value;
-    if (email.length > 15) {
-        seterror("email", "*Email length is too long");
-        returnval = false;
-    }
-
-    var phone = document.forms['myForm']["fphone"].value;
-    if (phone.length != 10) {
-        seterror("phone", "*Phone number should be of 10 digits!");
-        returnval = false;
-    }
-
-    var password = document.forms['myForm']["fpass"].value;
-    if (password.length < 6) {
-
-        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
-        seterror("pass", "*Password should be atleast 6 characters long!");
-        returnval = false;
-    }
-
-    var cpassword = document.forms['myForm']["fcpass"].value;
-    if (cpassword != password) {
-        seterror("cpass", "*Password and Confirm password should match!");
-        returnval = false;
-    }
-
-    return returnval;
 }
